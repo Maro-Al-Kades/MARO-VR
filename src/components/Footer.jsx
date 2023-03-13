c15e988b5e10707c2582ff97db9f46a40cc68a82
Maro-Al-Kades <97260148+Maro-Al-Kades@users.noreply.github.com>
2023-03-13 22:56:59 +0200

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -23,7 +23,7 @@ const Footer = () => {
             </span>
           </h1>
           <div className="mx-auto rounded-lg font-black mt-5 text-zinc-400 md:mt-12 md:max-w-lg text-center lg:text-lg">
-            <button className="" data-aos="fade-left" data-aos-offset="300">
+            <button className="" data-aos="fade-left">
               <a
                 href="#"
                 className="relative inline-flex items-center justify-center p-4 px-5 py-3 overflow-hidden font-medium text-indigo-600 rounded-md shadow-2xl group"
@@ -42,7 +42,6 @@ const Footer = () => {
       <hr
         className="text-green-200 mx-80"
         data-aos="fade-right"
-        data-aos-offset="300"
       />
       <footer className="bg-black pb-5">
         <div className="max-w-screen-xl px-4 pt-8 mx-auto sm:px-6 lg:px-8">
@@ -50,7 +49,6 @@ const Footer = () => {
             <p
               className="mt-4 font-bold font-primary text-center text-gray-400 lg:text-center lg:mt-0"
               data-aos="fade-up"
-              data-aos-offset="300"
             >
               DEVELOPED BY :{" "}
               <span className="cursor-pointer bg-clip-text text-transparent bg-gradient-to-r from-purple-400 via-emerald-400 to-green-500 font-semibold">
